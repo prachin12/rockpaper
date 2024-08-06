b67f0e42ce4a6cbb5c9e2d9df37e92e121dc5e98
prachin12 <magarprachin12@gmail.com>
2024-08-06 21:46:55 +1000

    Changed win and loose to no visibility
    working but still needs few updates

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let hs=0;
 let cs=0;
 
 btn.addEventListener('click', function(){
-r = rounds.value;
+r = rounds.value-1;
 console.log(r);
 });
 
@@ -19,12 +19,94 @@ btn1.addEventListener('click', function(){
 d = decision.value;
 console.log(d);
 let creturn= play(d);
+winner(d,creturn,r);
+r--;
 });
 
 function play(d){
+    let cd="";
 let num= Math.floor(Math.random()*3);
-if(num==0){cDecision.innerText='rock';}
-else if(num==1){cDecision.innerText='paper';}
-else{cDecision.innerText='scissors';}
+if(num==0){cDecision.innerText='rock';
+    cd='rock';
 }
+else if(num==1){cDecision.innerText='paper';
+    cd='paper';
+}
+else{cDecision.innerText='scissors';
+    cd='scissors';
+}
+return cd;
+}
+function winner(d,creturn,r)
+{
 
+    if(d=='rock' && creturn=='rock')
+    {
+        console.log('tie');
+       
+    }
+    else if(d=='rock' && creturn=='paper')
+    {
+        console.log('computer wins');
+        cs++;
+        cScore.innerText=cs;
+    }
+    else if(d=='rock' && creturn=='scissors')
+    {
+        console.log('you win');
+        hs++;
+        hScore.innerText=hs;
+    }
+    else if(d=='paper' && creturn=='rock')
+    {
+        console.log('you win');
+        hs++;
+        hScore.innerText=hs;
+    }
+    else if(d=='paper' && creturn=='paper')
+    {
+        console.log('tie');
+    }
+    else if(d=='paper' && creturn=='scissors')
+    {
+        console.log('computer wins');
+        cs++;
+        cScore.innerText=cs;
+    }
+    else if(d=='scissors' && creturn=='rock')
+    {
+        console.log('computer wins');
+        cs++;
+        cScore.innerText=cs;
+    }
+    else if(d=='scissors' && creturn=='paper')
+    {
+        console.log('you win');
+        hs++;
+        hScore.innerText=hs;
+    }
+    else
+    {
+        console.log('tie');
+    }
+
+    
+console.log(r);
+    if(r==0)
+    {
+        if(hs>cs)
+        {
+            alert('You win the game');
+        }
+        else if(hs<cs)
+        {
+            alert('Computer wins the game');
+        }
+        else
+        {
+            alert('Game is a tie');
+        }
+        
+    }
+    
+}
